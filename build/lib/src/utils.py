2a683a4d98aaa1d6dd6f289c135f7b3d6f2493ae
tawsyfbro <tawsifbro@gmail.com>
2024-05-01 19:41:49 +0600

    flask app

--- build/lib/src/utils.py
+++ build/lib/src/utils.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import pandas as pd
 import pickle
-
+import dill
 
 from src.exception import CustomException
 
@@ -62,7 +62,7 @@ def load_object(file_path):
 
         with open(file_path, 'rb') as file_obj:
 
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
